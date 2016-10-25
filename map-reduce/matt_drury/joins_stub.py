18a9371feddc48634c0acd2203052336c045dff0
Matthew Drury <matthew.drury@galvanize.com>
2016-10-25 08:37:32 -0700

    Add slides and more demo code.

--- map-reduce/matt_drury/joins_stub.py
+++ map-reduce/matt_drury/joins_stub.py
@@ -1,6 +1,4 @@
 from mrjob.job import MRJob
-from collections import defaultdict
-from itertools import product
 import json
 
 
@@ -30,28 +28,10 @@ class InnerJoin(MRJob):
     "date" attribute, which will be used as the join key.
     """
     def mapper(self, _, line):
-        jsn = json.loads(line)
-        key = jsn['key']
-        yield key, line
+        pass
 
     def reducer(self, key, json_group):
-        # Collect all the data together in a fixed data structure
-        tables = defaultdict(list)
-        table_names = set([])
-        for jsn in json_group:
-            jsn = json.loads(jsn)
-            tables[jsn['table']].append(jsn)
-            table_names.add(jsn['table'])
-        # Now join the data structures in a totally inefficient way : )
-        joined = []
-        for rows in product(*tables.values()):
-            if all_equal([row['date'] for row in rows]):
-                new_row = {}
-                extend_into_dict_from_dict(new_row, rows, blacklist=['table'])
-                joined.append(new_row)
-        # Now yield all the new data.
-        for row in joined:
-            yield (row, None)
+        pass
 
 
 if __name__ == '__main__':
