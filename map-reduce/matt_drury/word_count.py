0e85073283ac2682e929184af5920bea0d214dd9
Matthew Drury <matthew.drury@galvanize.com>
2016-10-26 13:15:11 -0700

    Clean up word_count.py

--- map-reduce/matt_drury/word_count.py
+++ map-reduce/matt_drury/word_count.py
@@ -1,13 +1,20 @@
 from mrjob.job import MRJob
+from string import punctuation
 
 class WordCount(MRJob):
+    """Hello World!"""
 
     def mapper(self, _, line):
         for word in line.split():
-            yield (word, 1)
+            for char in punctuation:
+                word = word.replace(char, "")
+            yield (word.replace(punctuation, ""), 1)
+
+    def combiner(self, word, counts):
+        yield(word, sum(counts))
 
     def reducer(self, word, counts):
-        yield ('{:09d}'.format(sum(counts)), word)
+        yield (word, sum(counts))
 
 
 if __name__ == '__main__':
