e4cc266e96d8f6fb176994c0e5a2372058b0491a
Ultramann <cary.goltermann@gmail.com>
2017-07-16 11:42:48 -0700

    Update remoteMongoSetup script. Add arguments for setup and usage docs

--- web-scraping/cary_goltermann/remoteMongoSetup.sh
+++ web-scraping/cary_goltermann/remoteMongoSetup.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+if [ "$1" == "-h" ] ; then
+    echo "$0 usage: to download, and setup a Mongo database on an EC2 instance for remote access.
+  Arguments:
+    1: name of database to create
+    2: name of user to create
+    3: password for user creation"
+    exit 0
+fi
+
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 
 sudo apt-get update
@@ -5,7 +16,7 @@ sudo apt-get install -y --allow-unauthenticated mongodb-org
 
 sudo service mongod start
 sleep 3
-mongo lecture --eval "db.createUser({user:'class',pwd:'gingerbreadman',roles:[{role:'readWrite',db:'lecture'}]})"
+mongo $1 --eval "db.createUser({user:'$2',pwd:'$3',roles:[{role:'readWrite',db:'$1'}]})"
 
 sudo tee /etc/mongod.conf >/dev/null <<EOF
 storage:
