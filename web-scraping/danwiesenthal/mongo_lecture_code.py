759b364c524cca0c9381873088a2eda475de3274
Daniel Wiesenthal <dw@cs.stanford.edu>
2017-09-11 12:55:13 -0700

    cleanup

--- web-scraping/danwiesenthal/mongo_lecture_code.py
+++ web-scraping/danwiesenthal/mongo_lecture_code.py
@@ -2,12 +2,15 @@
 import pymongo
 
 mongodb_uri = 'your_mongodb_uri_here (or set to None for localhost)'
-
-mc = pymongo.MongoClient(mongodb_uri) # Create MongoClient instance
-collection_name = 'mongodb_lecture' # Specify the collection name to work with
-db = mc[pymongo.uri_parser.parse_uri(mongodb_uri)['database']] # Specify the database to work with
-coll = db[collection_name] # Get a collection instance
+mc = pymongo.MongoClient(mongodb_uri)  # Create MongoClient instance
+db = mc[pymongo.uri_parser.parse_uri(mongodb_uri)['database']]  # Specify the database to work with
+collection_name = 'mongodb_lecture'  # Specify the collection name to work with
+coll = db[collection_name]  # Get a collection instance
 # Now we have a collection instance, ready to play with...
+
+# Clear up previous lecture cruft if needed:
+# result = coll.delete_many({})
+# print(result.deleted_count)
 
 # coll.find_one()
 # coll.find().count()
