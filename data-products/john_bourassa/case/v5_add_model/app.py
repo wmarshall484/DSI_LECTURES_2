8a1d42ff3896647f9112fd5c37137212c1516af6
Johink@gmail.com <johink@gmail.com>
2017-10-04 18:12:46 -0700

    Added John's data products lecture

---
+++ data-products/john_bourassa/case/v5_add_model/app.py
@@ -0,0 +1,29 @@
+from __future__ import division
+from flask import Flask, render_template, request, jsonify
+from math import sqrt
+from model import DataModel
+
+app = Flask(__name__)
+data_model = DataModel()
+
+@app.route('/')
+def index():
+    equations = data_model.get_prior_equations()
+    return render_template('quadratic.html', equations = equations)
+	
+@app.route('/solve', methods=['POST'])
+def solve():
+    user_data = request.json
+    a, b, c = int(user_data['xsquare']), int(user_data['x']), int(user_data['constant'])
+    root_1, root_2 = _solve_quadratic(a, b, c)
+    return jsonify({'root_1': root_1, 'root_2': root_2})
+
+
+def _solve_quadratic(a, b, c):
+    disc = b*b - 4*a*c
+    root_1 = (-b + sqrt(disc))/(2*a)
+    root_2 = (-b - sqrt(disc))/(2*a)
+    return root_1, root_2
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
