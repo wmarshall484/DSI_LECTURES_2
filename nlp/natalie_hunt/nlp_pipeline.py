d7a26922a8ffe0a57a9e120c0f9044c6387acbe2
nataliebeatrice <nataliebeatrice@gmail.com>
2018-04-17 09:43:30 -0400

    add natalie nlp

---
+++ nlp/natalie_hunt/nlp_pipeline.py
@@ -0,0 +1,65 @@
+ #!/usr/bin/python
+ # -*- coding: utf-8 -*-
+import string
+import unicodedata
+
+import nltk
+
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.util import ngrams
+from nltk import pos_tag
+from nltk import RegexpParser
+
+
+
+def extract_bow_from_raw_text(text_as_string):
+    """Extracts bag-of-words from a raw text string.
+
+    Parameters
+    ----------
+    text (str): a text document given as a string
+
+    Returns
+    -------
+    list : the list of the tokens extracted and filtered from the text
+    """
+    if (text_as_string == None):
+        return []
+
+    if (len(text_as_string) < 1):
+        return []
+
+    nfkd_form = unicodedata.normalize('NFKD', text_as_string)
+    text_input = str(nfkd_form.encode('ASCII', 'ignore'))
+
+    sent_tokens = sent_tokenize(text_input)
+
+    tokens = list(map(word_tokenize, sent_tokens))
+
+    sent_tags = list(map(pos_tag, tokens))
+
+    grammar = r"""
+        SENT: {<(J|N).*>}                # chunk sequences of proper nouns
+    """
+
+    cp = RegexpParser(grammar)
+    ret_tokens = list()
+    stemmer_snowball = SnowballStemmer('english')
+
+    for sent in sent_tags:
+        tree = cp.parse(sent)
+        for subtree in tree.subtrees():
+            if subtree.label() == 'SENT':
+                t_tokenlist = [tpos[0].lower() for tpos in subtree.leaves()]
+                t_tokens_stemsnowball = list(map(stemmer_snowball.stem, t_tokenlist))
+                #t_token = "-".join(t_tokens_stemsnowball)
+                #ret_tokens.append(t_token)
+                ret_tokens.extend(t_tokens_stemsnowball)
+            #if subtree.label() == 'V2V': print(subtree)
+    #tokens_lower = [map(string.lower, sent) for sent in tokens]
+
+    return(ret_tokens)
